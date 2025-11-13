2d51364e0257517e63a259609a8f0d55a6c97ba5
Mikhail Tiuterev <mike_tio@mail.ru>
2025-11-13 03:05:55 +0100

    updated config for the google analytics

---
+++ config.js
@@ -0,0 +1,12 @@
+// Configuration file - DO NOT COMMIT TO GITHUB
+// Add this file to .gitignore
+
+// Google Analytics Measurement ID
+// Get this from: https://analytics.google.com
+// Example: 'G-XXXXXXXXXX'
+window.ANALYTICS_ID = 'G-E89R47FNDF';
+
+// Formspree Form Endpoint
+// Get this from: https://formspree.io
+// Example: 'https://formspree.io/f/xyzabc123'
+window.FORMSPREE_ENDPOINT = 'https://formspree.io/f/xgvrvaoa';
