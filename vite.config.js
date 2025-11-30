2b43eaac8a0849f3cbcdd0024339184fb5fc2a9a
Mikhail Tiuterev <mike_tio@mail.ru>
2025-12-01 00:28:49 +0100

    aaa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/miketio.github.io/' // Replace with your repository name if different
+  base: '/miketio.github.io/', // Your repo name
 })
